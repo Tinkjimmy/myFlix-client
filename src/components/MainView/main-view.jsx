3d623e0534af53eef0cf02dc4fcbaa08820a185f
Tinkjimmy <tinkjimmy938@gmail.com>
2023-05-12 21:55:23 +0200

    adding api and proptypes

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -1,41 +1,42 @@
-import { useState} from "react";
+import { useState, useEffect} from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+
 
 export const MainView = () => {
 
 
-const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Interstellar",
-      description:"Astronauts embark on a mind-bending space mission to save humanity from a dying Earth.",
-      image:
-        "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
-      director: "Christopher Nolan",
-      genre: "sci-fi"
-    },
-    {
-      id: 2,
-      title: "Shrek",
-      description:"An ogre's hilarious journey to rescue a princess with the help of his witty sidekick, Donkey.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/7/7b/Shrek_%282001_animated_feature_film%29.jpg",
-      director: "Vicky Jenson",
-      genre: "comedy"
-    },
-    {
-      id: 3,
-      title: "Flubber",
-      description:"A quirky scientist invents a bouncy substance that leads to chaotic adventures and unexpected consequences.",
-      image:
-        "https://cdn.s7.shopdisney.eu/is/image/DisneyStoreES/466043420388-2?fmt=jpeg&qlt=90&wid=652&hei=652&defaultImage=no-image-it_it",
-      director: "Les Mayfield",
-      genre: "comedy"
-    }
-    
-  ]);
+const [movies, setMovies] = useState([]);
 const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => { fetch("https://movie-api-1000.herokuapp.com/")
+   .then((response) => response.json())
+   .then((data) => {
+     const moviesFromApi = data.docs.map((doc) => {
+       return{
+         id: doc._id,
+         description: doc.Description,
+         title: doc.Title,
+         image:`https://covers.openlibrary.org/b/id/${doc.ImagePath}-L.jpg`,
+         director: doc.Director_Name?.[0],
+         genre: doc.Genre_Name
+       };
+     });
+     setMovies(moviesFromApi);
+
+   });
+   }, []);
+
+
+
+
+
+
+
+
+
+
+
 
  if (selectedMovie) {
    return ( <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,20 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({movie, onMovieClick}) => {
   return ( 
     <div  onClick={() => {onMovieClick(movie);}}>
       {movie.title}
     </div>
     );
+  };
+
+  MovieCard.propTypes ={
+    movies: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string,
+      image: PropTypes.string.isRequired,
+      director: PropTypes.string,
+      genre: PropTypes.string
+    }).isRequired,
+    onBookClick: PropTypes.func.isRequired
   };
