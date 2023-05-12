b6eb8ced864d48596cfd29c531092626aa6b635d
Tinkjimmy <tinkjimmy938@gmail.com>
2023-05-12 22:19:09 +0200

    making changes to myFlix client

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,25 +2,36 @@ export const MovieView = ({movie,onBackClick}) => {
   return (
     <div>
         <div>
-          <img src= {movie.image} />
+          <img src= {movie.ImagePath} />
         </div>
         <div>
           <span>Title: </span>
-          <span>{movie.title}</span>
+          <span>{movie.Title}</span>
         </div>
         <div>
           <span>Description: </span>
-          <span>{movie.description}</span>
+          <span>{movie.Description}</span>
         </div>
         <div>
           <span>Genre: </span>
-          <span>{movie.genre}</span>
+          <span>{movie.Genre_Name}</span>
         </div>
         <div>
           <span>Director: </span>
-          <span>{movie.director}</span>
+          <span>{movie.Director_Name}</span>
         </div>
         <button onClick={onBackClick}>Back</button>
      </div>
     );
+  };
+
+  MovieView.propTypes ={
+    movies: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string,
+      image: PropTypes.string.isRequired,
+      director: PropTypes.string,
+      genre: PropTypes.string
+    }).isRequired,
+    onBookClick: PropTypes.func.isRequired
   };
