2c32c05b722ed2abbb443ddba4791ffb95946827
Tinkjimmy <tinkjimmy938@gmail.com>
2023-05-25 07:09:12 +0200

    introducing routing

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,11 +1,17 @@
 import PropTypes from "prop-types";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import "./movie-view.scss";
 
-export const MovieView = ({movie,onBackClick}) => {
+export const MovieView = ({movies}) => {
+  const { movieId } = useParams();
+
+  const movie = movies.find((m) => m.id === movieId);
+
   return (
     <div>
       <div>
-        <img src={movie.image} />
+        <img className="w-100" src={movie.image} />
       </div>
       <div>
         <span>Title: </span>
@@ -23,9 +29,9 @@ export const MovieView = ({movie,onBackClick}) => {
         <span>Director: </span>
         <span>{movie.director}</span>
       </div>
-      <button onClick={onBackClick}  className="back-button" style={{ cursor: "pointer" }}>
-        Back
-        </button>
+      <Link to={`/`}>
+        <button className="back-button">Back</button>
+      </Link>
     </div>
   );
 };
