e0ed4ec02538cc3c94e9861fd36a4780f3b539e3
Tinkjimmy <tinkjimmy938@gmail.com>
2023-06-12 18:26:16 +0200

    bo

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -49,7 +49,7 @@ export const MainView = () => {
 
   //preferiti filtro
   console.log("film");
-  const favourites = user.Favourites;
+  // const favourites = user.Favourites;
   const films = movies;
 
   const favMovies = films.filter((film) => favourites.includes(film.id));
